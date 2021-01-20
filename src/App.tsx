8a250558d334f8bbe6ffdcc679fc73f8fb880990
정다운 <dkun9039@paycrux.co.kr>
2021-01-21 00:10:18 +0900

    feat: 프로젝트 초기설정 - eslint, prettier 적용 - 기본 path 및 env 설정 - index.html, index.js, app.js 생성

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>사랑의언어 테스트</div>;
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+import * as serviceWorker from "../serviceWorker";
+
+ReactDom.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
